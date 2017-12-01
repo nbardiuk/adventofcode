18c5e5bd4db78afd6043e59238011d16d5422794
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2017-12-01 18:58:46 +0000

    day01 simplified

--- scadvent/src/main/scala/day01/InverseCaptcha.scala
+++ scadvent/src/main/scala/day01/InverseCaptcha.scala
@@ -1,26 +1,16 @@
 package day01
 
 import scala.Function.tupled
-import scala.annotation.tailrec
 
 object InverseCaptcha {
-  def sumMatchesHalfwayAround(number: String): Int =
-    sumMatches(number.length / 2, number)
+  def sumMatchesHalfwayAround(digits: String): Int =
+    sumMatchesAt(digits.length / 2)(digits)
 
-  def sumMatchesNext(number: String): Int = sumMatches(1, number)
+  def sumMatchesNext(digits: String): Int = sumMatchesAt(1)(digits)
 
-  private def sumMatches(distance: Int, number: String) = {
-    val digits = toDigits(number)
-    val toMatch = shift(digits, distance)
-    val matches = digits.zip(toMatch).filter(tupled(_ == _)).map(_._1)
-    matches.sum
-  }
+  private def sumMatchesAt(distance: Int)(digits: String): Int =
+    digits.zip(shift(digits, distance)).filter(tupled(_ == _)).map(_._1.asDigit).sum
 
-  private def toDigits(number: String): Seq[Int] =
-    number.toCharArray.map(code => code - '0')
-
-  @tailrec
-  private def shift[T](seq: Seq[T], times: Int): Seq[T] =
-    if (times == 0 || seq.isEmpty) seq
-    else shift(seq.tail :+ seq.head, times - 1)
+  private def shift[T](seq: Seq[T], distance: Int): Seq[T] =
+    seq.drop(distance) ++ seq.take(distance)
 }
