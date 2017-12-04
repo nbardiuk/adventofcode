3fe0367e62f0c548487c4fcdce874e9a4a597669
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2017-12-04 21:08:38 +0000

    day04 solution to part 2

--- src/main/scala/day04/Passphrase.scala
+++ src/main/scala/day04/Passphrase.scala
@@ -1,11 +1,34 @@
 package day04
 
 object Passphrase {
-  def countValid(passphrases: Seq[String]): Int = passphrases.count(valid)
 
-  def valid(passphrase: String): Boolean = {
-    val words = passphrase.split(' ')
-    words.toSet.size == words.length
+  def countUnique(passphrases: Seq[String]): Int = passphrases.count(unique)
+
+  def unique(passphrase: String): Boolean = unique(words(passphrase))
+
+  def countWithoutAnagrams(passphrases: Seq[String]): Int =
+    passphrases.count(withoutAnagrams)
+
+  def withoutAnagrams(passphrase: String): Boolean =
+    unique(words(passphrase).map(Anagram))
+
+  private def words(passphrase: String) = passphrase.split(' ')
+
+  private def unique[T](ts: Seq[T]): Boolean = ts.toSet.size == ts.size
+
+  case class Anagram(word: String) {
+
+    private lazy val letters = word.groupBy(c => c).mapValues(_.length)
+
+    def eq(o: Anagram): Boolean = letters == o.letters
+
+    override def hashCode(): Int = letters.hashCode()
+
+    override def equals(other: scala.Any): Boolean = other match {
+      case a: Anagram => eq(a)
+      case _          => false
+    }
+
   }
 
 }
--- src/test/scala/day04/PassphraseTest.scala
+++ src/test/scala/day04/PassphraseTest.scala
@@ -1,6 +1,11 @@
 package day04
 
-import day04.Passphrase.{countValid, valid}
+import day04.Passphrase.{
+  countUnique,
+  countWithoutAnagrams,
+  unique,
+  withoutAnagrams
+}
 import org.scalatest.FlatSpec
 
 import scala.io.Source.fromURL
@@ -8,23 +13,37 @@ import scala.io.Source.fromURL
 class PassphraseTest extends FlatSpec {
 
   "Passphrase with unique words" should "be valid" in {
-    assert(valid("aa bb cc dd"))
-    assert(valid("aa aaa"))
+    assert(unique("aa bb cc dd"))
+    assert(unique("aa aaa"))
   }
   "Passphrase with duplicate words" should "be invalid" in {
-    assert(!valid("aa aa"))
+    assert(!unique("aa aa"))
   }
 
-  "Count valid" should "count valid passphrases" in {
-    assert(countValid(Seq("aa aaa", "a aa", "aa aa")) == 2)
+  "Count unique" should "count valid passphrases" in {
+    assert(countUnique(Seq("aa aaa", "a aa", "aa aa")) == 2)
   }
   it should "count my input" in {
-    assert(countValid(myInput) == 337)
+    assert(countUnique(myInput) == 337)
   }
 
-  private def myInput: Seq[String] =
-    fromURL(getClass.getResource("/day04/input"))
-      .getLines()
-      .toSeq
+  "Passphrase without anagrams" should "be valid" in {
+    assert(withoutAnagrams("abc def"))
+    assert(withoutAnagrams("a ab abc abd abf abj"))
+    assert(withoutAnagrams("iiii oiii ooii oooi oooo"))
+  }
+  "Passphrase with anagrams" should "be invalid" in {
+    assert(!withoutAnagrams("abc cba"))
+  }
+
+  "Count without anagrams" should "count passphrases without anagrams" in {
+    assert(countWithoutAnagrams(Seq("aa aaa", "ab aab", "ab ba")) == 2)
+  }
+  it should "count my input" in {
+    assert(countWithoutAnagrams(myInput) == 231)
+  }
+
+  private lazy val myInput: Seq[String] =
+    fromURL(getClass.getResource("/day04/input")).getLines().toSeq
 
 }
