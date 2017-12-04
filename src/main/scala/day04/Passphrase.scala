35ce81cf21313e70007596b397229035a18bd05b
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2017-12-04 21:10:25 +0000

    day04 removed extra class

--- src/main/scala/day04/Passphrase.scala
+++ src/main/scala/day04/Passphrase.scala
@@ -10,25 +10,10 @@ object Passphrase {
     passphrases.count(withoutAnagrams)
 
   def withoutAnagrams(passphrase: String): Boolean =
-    unique(words(passphrase).map(Anagram))
+    unique(words(passphrase).map(_.groupBy(c => c)))
 
   private def words(passphrase: String) = passphrase.split(' ')
 
   private def unique[T](ts: Seq[T]): Boolean = ts.toSet.size == ts.size
-
-  case class Anagram(word: String) {
-
-    private lazy val letters = word.groupBy(c => c).mapValues(_.length)
-
-    def eq(o: Anagram): Boolean = letters == o.letters
-
-    override def hashCode(): Int = letters.hashCode()
-
-    override def equals(other: scala.Any): Boolean = other match {
-      case a: Anagram => eq(a)
-      case _          => false
-    }
-
-  }
 
 }
