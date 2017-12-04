4428d4b5d3826ee2fb9bb26d96dc3953ee79353b
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2017-12-04 21:13:00 +0000

    day04 naming and formatting

--- src/main/scala/day04/Passphrase.scala
+++ src/main/scala/day04/Passphrase.scala
@@ -6,11 +6,11 @@ object Passphrase {
 
   def unique(passphrase: String): Boolean = unique(words(passphrase))
 
-  def countWithoutAnagrams(passphrases: Seq[String]): Int =
-    passphrases.count(withoutAnagrams)
+  def countWithoutAnagrams(passphrases: Seq[String]): Int = passphrases.count(withoutAnagrams)
 
-  def withoutAnagrams(passphrase: String): Boolean =
-    unique(words(passphrase).map(_.groupBy(c => c)))
+  def withoutAnagrams(passphrase: String): Boolean = unique(words(passphrase).map(anagramId))
+
+  private def anagramId(word: String) = word.groupBy(c => c)
 
   private def words(passphrase: String) = passphrase.split(' ')
 
