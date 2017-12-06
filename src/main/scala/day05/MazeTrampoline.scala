0d1e57b058340dff31c9d547110669215493937c
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2017-12-06 00:43:13 +0000

    day05 small change

--- src/main/scala/day05/MazeTrampoline.scala
+++ src/main/scala/day05/MazeTrampoline.scala
@@ -2,26 +2,27 @@ package day05
 
 import scala.Function.tupled
 import scala.collection.immutable.Stream.iterate
+import scala.collection.mutable
 
 object MazeTrampoline {
-  def countStrangeJumps(commands: Array[Int]): Int = {
+  def countStrangeJumps(commands: mutable.IndexedSeq[Int]): Int = {
     iterate((commands, 0))(tupled(strangeJump)).takeWhile {
       case (coms, cursor) => coms.indices contains cursor
     }.size
   }
 
-  def strangeJump(commands: Array[Int], cursor: Int): (Array[Int], Int) = {
+  def strangeJump(commands: mutable.IndexedSeq[Int], cursor: Int): (mutable.IndexedSeq[Int], Int) = {
     val command = commands(cursor)
     commands(cursor) = command + (if (command >= 3) -1 else 1)
     (commands, command + cursor)
   }
 
-  def countJumps(commands: Array[Int]): Int =
+  def countJumps(commands: mutable.IndexedSeq[Int]): Int =
     iterate((commands, 0))(tupled(jump)).takeWhile {
       case (coms, cursor) => coms.indices contains cursor
     }.size
 
-  def jump(commands: Array[Int], cursor: Int): (Array[Int], Int) = {
+  def jump(commands: mutable.IndexedSeq[Int], cursor: Int): (mutable.IndexedSeq[Int], Int) = {
     val command = commands(cursor)
     commands(cursor) = command + 1
     (commands, command + cursor)
