963ce25e654d29049f208c362ca1d04b8d5ce467
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-12-02 14:29:21 +0000

    more descriptive test names

--- 2018/tests/Day1Test.hs
+++ 2018/tests/Day1Test.hs
@@ -3,17 +3,19 @@ module Day1Test where
 import           Day1
 import           Test.Tasty.Hspec
 
-spec_part1 = do
-  it "example 1" $ part1 ["+1", "+1", "+1"] `shouldBe` 3
-  it "example 2" $ part1 ["+1", "+1", "-2"] `shouldBe` 0
-  it "example 3" $ part1 ["-1", "-2", "-3"] `shouldBe` (-6)
-  it "my input" $ (part1 <$> readInput) >>= (`shouldBe` 474)
+spec_Day1 = do
 
-spec_part2 = do
-  it "example 1" $ part2 ["+1", "-1"] `shouldBe` 0
-  it "example 2" $ part2 ["+3", "+3", "+4", "-2", "-4"] `shouldBe` 10
-  it "example 3" $ part2 ["-6", "+3", "+8", "+5", "-6"] `shouldBe` 5
-  it "example 4" $ part2 ["+7", "+7", "-2", "-7", "-4"] `shouldBe` 14
-  it "my input" $ (part2 <$> readInput) >>= (`shouldBe` 137041)
+  describe "Part 1" $ do
+    it "example 1" $ part1 ["+1", "+1", "+1"] `shouldBe` 3
+    it "example 2" $ part1 ["+1", "+1", "-2"] `shouldBe` 0
+    it "example 3" $ part1 ["-1", "-2", "-3"] `shouldBe` (-6)
+    it "my input" $ (part1 <$> readInput) >>= (`shouldBe` 474)
+
+  describe "Part 2" $ do
+    it "example 1" $ part2 ["+1", "-1"] `shouldBe` 0
+    it "example 2" $ part2 ["+3", "+3", "+4", "-2", "-4"] `shouldBe` 10
+    it "example 3" $ part2 ["-6", "+3", "+8", "+5", "-6"] `shouldBe` 5
+    it "example 4" $ part2 ["+7", "+7", "-2", "-7", "-4"] `shouldBe` 14
+    it "my input" $ (part2 <$> readInput) >>= (`shouldBe` 137041)
 
 readInput = lines <$> readFile "data/day1.csv"
--- 2018/tests/Day2Test.hs
+++ 2018/tests/Day2Test.hs
@@ -3,24 +3,33 @@ module Day2Test where
 import           Day2
 import           Test.Tasty.Hspec
 
-spec_part1 = do
+spec_Day2 = do
 
-  it "example" $ do
-    let example =
-          ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
-    part1 example `shouldBe` 12
+  describe "Part 1" $ do
 
-  it "my input" $ (part1 <$> readInput) >>= (`shouldBe` 6200)
+    it "example" $ do
+      let example =
+            [ "abcdef"
+            , "bababc"
+            , "abbcde"
+            , "abcccd"
+            , "aabcdd"
+            , "abcdee"
+            , "ababab"
+            ]
+      part1 example `shouldBe` 12
 
-spec_part2 = do
+    it "my input" $ (part1 <$> readInput) >>= (`shouldBe` 6200)
 
-  it "example" $ do
-    let example =
-          ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
-    part2 example `shouldBe` "fgij"
+  describe "Part 2" $ do
 
-  it "my input"
-    $   (part2 <$> readInput)
-    >>= (`shouldBe` "xpysnnkqrbuhefmcajodplyzw")
+    it "example" $ do
+      let example =
+            ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+      part2 example `shouldBe` "fgij"
+
+    it "my input"
+      $   (part2 <$> readInput)
+      >>= (`shouldBe` "xpysnnkqrbuhefmcajodplyzw")
 
 readInput = lines <$> readFile "data/day2.csv"
