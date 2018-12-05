eeb35e2b0a71f16fd39343d92c93af7191aed762
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-12-05 22:29:03 +0000

    speedup Day3 from 300ms to 120ms

--- 2018/src/Day3.hs
+++ 2018/src/Day3.hs
@@ -1,42 +1,52 @@
 module Day3 where
 
-import           Data.List                  (findIndex)
-import qualified Data.Map.Strict            as Map
+import           Data.List                  (find, inits)
 import           Data.Maybe                 (fromJust)
+import qualified Data.Set                   as Set
 import           Data.Void                  (Void)
 import           Text.Megaparsec            (Parsec, parseMaybe)
 import           Text.Megaparsec.Char       (char, string)
 import           Text.Megaparsec.Char.Lexer (decimal)
 
 part1 :: [String] -> Int
-part1 = countOverlaps . union . readClaims
- where
-  countOverlaps = Map.size . Map.filter (> 1)
-  union         = Map.unionsWith (+)
+part1 = Set.size . overlaps . (readClaim <$>)
 
 part2 :: [String] -> Int
-part2 input = maybe 0 (+ 1) $ findIndex withoutOverlap unionSections
+part2 = maybe 0 claimId . firstWithoutOverlap . cycles . (readClaim <$>)
  where
-  withoutOverlap = Map.null . Map.filter (> 1)
-  unionSections  = Map.intersection union <$> claims
-  union          = Map.unionsWith (+) claims
-  claims         = readClaims input
+  claimId (i, _, _, _) = i
+  firstWithoutOverlap = (head <$>) . find withoutOverlap
+  withoutOverlap (claim : rest) = all (not . hasOverlap claim) rest
+  cycles as =
+    let len = length as
+    in  (\n -> take len $ drop n $ cycle as) <$> [0 .. len - 1]
 
-readClaims :: [String] -> [Map.Map (Int, Int) Int]
-readClaims = (Map.fromList <$>) . (points <$>) . (readClaim <$>)
+overlaps :: [Claim] -> Set.Set Point
+overlaps claims =
+  Set.unions $ uncurry ((<$>) . overlapPoints) =<< zip claims (inits claims)
 
-type Claim = (Int, Int, Int, Int, Int)
-type Point = ((Int, Int), Int)
+overlapPoints :: Claim -> Claim -> Set.Set Point
+overlapPoints c1 c2 = case c1 `overlapRect` c2 of
+  ((a, b), (c, d)) -> Set.fromList [ (i, j) | i <- [a .. b], j <- [c .. d] ]
 
-points :: Claim -> [Point]
-points (_, x, y, w, h) =
-  [ ((i, j), 1) | i <- [x .. (x + w - 1)], j <- [y .. (y + h - 1)] ]
+hasOverlap :: Claim -> Claim -> Bool
+hasOverlap c1 c2 = case c1 `overlapRect` c2 of
+  ((a, b), (c, d)) -> a <= b && c <= d
+
+overlapRect :: Claim -> Claim -> (Point, Point)
+overlapRect (_, (x1, y1), w1, h1) (_, (x2, y2), w2, h2) =
+  ( (max x1 x2, min (x1 + w1 - 1) (x2 + w2 - 1))
+  , (max y1 y2, min (y1 + h1 - 1) (y2 + h2 - 1))
+  )
+
+type Claim = (Int, Point, Int, Int)
+type Point = (Int, Int)
 
 readClaim :: String -> Claim
-readClaim = fromJust . parseMaybe claim
+readClaim = fromJust . parseMaybe claimP
 
-claim :: Parsec Void String Claim
-claim = do
+claimP :: Parsec Void String Claim
+claimP = do
   _ <- char '#'
   i <- decimal
   _ <- string " @ "
@@ -47,4 +57,4 @@ claim = do
   w <- decimal
   _ <- char 'x'
   h <- decimal
-  return (i, x, y, w, h)
+  return (i, (x, y), w, h)
