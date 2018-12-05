d6c16beb344b40fdd6c371d41069c6373e352514
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-12-05 22:32:50 +0000

    fix wrong rectangle coordinates

--- 2018/src/Day3.hs
+++ 2018/src/Day3.hs
@@ -27,16 +27,16 @@ overlaps claims =
 
 overlapPoints :: Claim -> Claim -> Set.Set Point
 overlapPoints c1 c2 = case c1 `overlapRect` c2 of
-  ((a, b), (c, d)) -> Set.fromList [ (i, j) | i <- [a .. b], j <- [c .. d] ]
+  ((x1, y1), (x2, y2)) -> Set.fromList [ (i, j) | i <- [x1 .. x2], j <- [y1 .. y2] ]
 
 hasOverlap :: Claim -> Claim -> Bool
 hasOverlap c1 c2 = case c1 `overlapRect` c2 of
-  ((a, b), (c, d)) -> a <= b && c <= d
+  ((x1, y1), (x2, y2)) -> x1 <= x2 && y1 <= y2
 
 overlapRect :: Claim -> Claim -> (Point, Point)
 overlapRect (_, (x1, y1), w1, h1) (_, (x2, y2), w2, h2) =
-  ( (max x1 x2, min (x1 + w1 - 1) (x2 + w2 - 1))
-  , (max y1 y2, min (y1 + h1 - 1) (y2 + h2 - 1))
+  ( (max x1 x2                      , max y1 y2)
+  , (min (x1 + w1 - 1) (x2 + w2 - 1), min (y1 + h1 - 1) (y2 + h2 - 1))
   )
 
 type Claim = (Int, Point, Int, Int)
