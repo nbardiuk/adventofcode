821cf5440c0db177a69e758d2309b4759a7a929d
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-12-09 19:20:20 +0000

    improve performance day9 from 6s to 4s

--- 2018/src/Day9.hs
+++ 2018/src/Day9.hs
@@ -9,53 +9,24 @@ import           Text.Megaparsec                ( Parsec
 import           Text.Megaparsec.Char           ( string )
 import           Text.Megaparsec.Char.Lexer     ( decimal )
 
+import           Data.List                      ( foldl' )
+
 part1 :: String -> Int
-part1 = maxScore gameScores . readIn
+part1 = maxScore . readIn
 
-gameScores :: [Int]
-gameScores = snd <$> iterate gameStep (initBoard, 0)
-
-maxScore :: [Int] -> (Int, Int) -> Int
-maxScore scores (players, steps) =
-  maximum . Map.elems $ Map.fromListWith (+) $ take (steps + 1) $ zip
-    (cycle [1 .. players])
-    scores
-
-gameStep :: (Board, Int) -> (Board, Int)
-gameStep (board, _) =
-  let next = 1 + step board
-      c    = marbles board
-  in  if next `mod` 23 == 0
-        then let nc = rotate (-7) c in (Board (pop nc) next, next + (focus nc))
-        else (Board (append next $ rotate 1 c) next, 0)
-
-data Board = Board {marbles:: Circle Int, step:: Int} deriving Show
-initBoard = Board (Circle 0 [] 0 [] 0) 0
-
-data Circle a = Circle {leftLen:: Int, left:: [a], focus:: a, right:: [a], rightLen:: Int} deriving Show
-
-rotate :: Int -> Circle Int -> Circle Int
-rotate n c@(Circle lc l f r rc)
-  | n == 0 = c
-  | len c <= abs n = rotate ((sign n) * (abs n `mod` len c)) c
-  | rc < n = rotate n $ leanRight c
-  | n < (-lc) = rotate n $ leanLeft c
-  | 0 < n = rotate (n - 1) (Circle (lc + 1) (f : l) (head r) (tail r) (rc - 1))
-  | n < 0 = rotate (n + 1) (Circle (lc - 1) (tail l) (head l) (f : r) (rc + 1))
-
-len (Circle lc _ _ _ rc) = lc + 1 + rc
-sign i = if i >= 0 then 1 else -1
-
-append v (Circle lc l f r rc) = Circle (lc + 1) (f : l) v r rc
-
-pop c@(Circle lc l _ r rc) =
-  if rc > 0 then Circle lc l (head r) (tail r) (rc - 1) else pop $ leanRight c
-
--- | lean circle to the right
-leanRight (Circle lc l f r rc) = Circle 0 [] f (r ++ reverse l) (lc + rc)
-
--- | lean circle to the left
-leanLeft (Circle lc l f r rc) = Circle (lc + rc) (l ++ reverse r) f [] 0
+maxScore :: (Int, Int) -> Int
+maxScore (players, steps) = maximum . Map.elems $ fst $ foldl'
+  move
+  (Map.empty, singleton 0)
+  [1 .. steps]
+ where
+  move (score, board) i =
+    let back    = rotate (-7) board
+        forward = rotate 1 board
+        player  = i `mod` players
+    in  if i `mod` 23 == 0
+          then (Map.insertWith (+) player (i + focus back) score, delete back)
+          else (score, insertRight i forward)
 
 readIn :: String -> (Int, Int)
 readIn = fromJust . parseMaybe pairP
@@ -65,3 +36,44 @@ pairP = do
   players <- decimal <* string " players;"
   points  <- string " last marble is worth " *> decimal <* string " points"
   return (players, points)
+
+-----------------------------------------------------------------------------------
+-- Circle
+
+data Circle a = Circle {leftLen:: Int, left:: [a], focus:: a, right:: [a], rightLen:: Int}
+
+singleton :: a -> Circle a
+singleton a = Circle 0 [] a [] 0
+
+rotate :: Int -> Circle a -> Circle a
+rotate n c@(Circle lc l f r rc)
+  | n == 0 = c
+  | len c <= abs n = rotate (sign n * (abs n `mod` len c)) c
+  | rc < n = rotate n $ leanRight c
+  | n < (-lc) = rotate n $ leanLeft c
+  | 0 < n = rotate (n - 1) (Circle (lc + 1) (f : l) (head r) (tail r) (rc - 1))
+  | n < 0 = rotate (n + 1) (Circle (lc - 1) (tail l) (head l) (f : r) (rc + 1))
+
+len :: Circle a -> Int
+len (Circle lc _ _ _ rc) = lc + 1 + rc
+
+sign :: Int -> Int
+sign i = if i >= 0 then 1 else -1
+
+insertRight :: a -> Circle a -> Circle a
+insertRight v (Circle lc l f r rc) = Circle (lc + 1) (f : l) v r rc
+
+delete :: Circle a -> Circle a
+delete c@(Circle lc l _ r rc) = if rc > 0
+  then Circle lc l (head r) (tail r) (rc - 1)
+  else delete $ leanRight c
+
+-- | lean circle to the right
+leanRight :: Circle a -> Circle a
+leanRight (Circle lc l f r rc) = Circle 0 [] f (r ++ reverse l) (lc + rc)
+
+-- | lean circle to the left
+leanLeft :: Circle a -> Circle a
+leanLeft (Circle lc l f r rc) = Circle (lc + rc) (l ++ reverse r) f [] 0
+
+-----------------------------------------------------------------------------------
