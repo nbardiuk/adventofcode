67c5b664cb2662d44d98fb2200729f240769a88c
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-02 09:06:03 +0000

    Day 2 benchmark

--- 2019/benches/bench.rs
+++ 2019/benches/bench.rs
@@ -8,5 +8,13 @@ pub fn day01(c: &mut Criterion) {
     c.bench_function("day01::part2", |b| b.iter(|| part2(input)));
 }
 
-criterion_group!(benches, day01);
+pub fn day02(c: &mut Criterion) {
+    use aoc2019::day02::{part1, part2};
+    let input = include_str!("../res/day02.txt");
+
+    c.bench_function("day02::part1", |b| b.iter(|| part1(input)));
+    c.bench_function("day02::part2", |b| b.iter(|| part2(input)));
+}
+
+criterion_group!(benches, day01, day02);
 criterion_main!(benches);
