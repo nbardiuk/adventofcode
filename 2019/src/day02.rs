31cac3d0ccdb22152a49e56cc77558ed62ca8430
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-02 09:04:11 +0000

    Day 2 cleanup

--- 2019/src/day02.rs
+++ 2019/src/day02.rs
@@ -3,9 +3,11 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
+    let memory = numbers(input);
+
     for noun in 0..99 {
         for verb in 0..99 {
-            if 19_690_720 == program(noun, verb, numbers(input)) {
+            if 19_690_720 == program(noun, verb, memory.clone()) {
                 return noun * 100 + verb;
             }
         }
@@ -21,36 +23,32 @@ fn program(noun: usize, verb: usize, mut memory: Vec<usize>) -> usize {
 
 fn numbers(text: &str) -> Vec<usize> {
     text.split(',')
-        .filter_map(|line| line.parse::<usize>().ok())
+        .filter_map(|num| num.parse::<usize>().ok())
         .collect()
 }
 
 fn execute(memory: Vec<usize>) -> Vec<usize> {
-    fn go(memory: Vec<usize>, counter: usize) -> Vec<usize> {
-        match iteration(memory, counter) {
+    fn go(memory: Vec<usize>, pointer: usize) -> Vec<usize> {
+        match iteration(memory, pointer) {
             (memory, None) => memory,
-            (memory, Some(counter)) => go(memory, counter),
+            (memory, Some(pointer)) => go(memory, pointer),
         }
     };
     go(memory, 0)
 }
 
-fn iteration(mut memory: Vec<usize>, counter: usize) -> (Vec<usize>, Option<usize>) {
-    match memory[counter] {
-        1 => {
-            let a = memory[counter + 1];
-            let b = memory[counter + 2];
-            let c = memory[counter + 3];
-            memory[c] = memory[a] + memory[b];
-            (memory, Some(counter + 4))
-        }
-        2 => {
-            let a = memory[counter + 1];
-            let b = memory[counter + 2];
-            let c = memory[counter + 3];
-            memory[c] = memory[a] * memory[b];
-            (memory, Some(counter + 4))
-        }
+fn iteration(memory: Vec<usize>, pointer: usize) -> (Vec<usize>, Option<usize>) {
+    fn eval(op: fn(usize, usize) -> usize, mut memory: Vec<usize>, pointer: usize) -> Vec<usize> {
+        let a = memory[memory[pointer + 1]];
+        let b = memory[memory[pointer + 2]];
+        let dest = memory[pointer + 3];
+        memory[dest] = op(a, b);
+        memory
+    };
+
+    match memory[pointer] {
+        1 => (eval(|a, b| a + b, memory, pointer), Some(pointer + 4)),
+        2 => (eval(|a, b| a * b, memory, pointer), Some(pointer + 4)),
         _ => (memory, None),
     }
 }
@@ -62,51 +60,38 @@ mod spec {
 
     #[test]
     fn iteration_addition() {
-        let memory = vec![1, 0, 0, 0, 99];
-        let counter = 0;
-
-        let (memory, counter) = iteration(memory, counter);
+        let (memory, pointer) = iteration(vec![1, 0, 0, 0, 99], 0);
 
         assert_eq!(memory, [2, 0, 0, 0, 99]);
-        assert_eq!(counter, Some(4));
+        assert_eq!(pointer, Some(4));
     }
 
     #[test]
     fn iteration_multiplication() {
-        let memory = vec![2, 3, 0, 3, 99];
-        let counter = 0;
-
-        let (memory, counter) = iteration(memory, counter);
+        let (memory, pointer) = iteration(vec![2, 3, 0, 3, 99], 0);
 
         assert_eq!(memory, [2, 3, 0, 6, 99]);
-        assert_eq!(counter, Some(4));
+        assert_eq!(pointer, Some(4));
     }
 
     #[test]
     fn iteration_halting() {
-        let memory = vec![99];
-        let counter = 0;
-
-        let (memory, counter) = iteration(memory, counter);
+        let (memory, pointer) = iteration(vec![99], 0);
 
         assert_eq!(memory, [99]);
-        assert_eq!(counter, None);
+        assert_eq!(pointer, None);
     }
 
     #[test]
     fn execution_halting() {
-        let memory = vec![2, 4, 4, 5, 99, 0];
-
-        let memory = execute(memory);
+        let memory = execute(vec![2, 4, 4, 5, 99, 0]);
 
         assert_eq!(memory, [2, 4, 4, 5, 99, 9801]);
     }
 
     #[test]
     fn execution_overriding_instruction() {
-        let memory = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
-
-        let memory = execute(memory);
+        let memory = execute(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]);
 
         assert_eq!(memory, [30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }
