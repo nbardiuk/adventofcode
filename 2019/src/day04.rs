a090be6056be5936100c62898f0f00a1ce4b2295
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-04 16:55:59 +0000

    Day4 part2

--- 2019/src/day04.rs
+++ 2019/src/day04.rs
@@ -3,28 +3,56 @@ pub const INPUT: &str = "171309-643603";
 pub fn part1(input: &str) -> usize {
     if let [from, to] = input.split('-').collect::<Vec<_>>().as_slice() {
         if let (Ok(from), Ok(to)) = (from.parse(), to.parse()) {
-            return (from..=to).filter(|&n| matches_password(n)).count();
+            return (from..=to).filter(|&n| matches_part1(n)).count();
+        }
+    };
+    0
+}
+pub fn part2(input: &str) -> usize {
+    if let [from, to] = input.split('-').collect::<Vec<_>>().as_slice() {
+        if let (Ok(from), Ok(to)) = (from.parse(), to.parse()) {
+            return (from..=to).filter(|&n| matches_part2(n)).count();
         }
     };
     0
 }
 
-fn matches_password(number: usize) -> bool {
-    let (growing, has_dups, _) =
-        digits(number)
-            .iter()
-            .fold(
-                (false, false, None),
-                |(growing, has_dups, prev), digit| match prev {
-                    Some(before) => (
-                        growing && before <= digit,
-                        has_dups || before == digit,
-                        Some(digit),
-                    ),
-                    None => (true, false, Some(digit)),
-                },
-            );
-    growing && has_dups
+fn matches_part1(number: usize) -> bool {
+    let mut prev = 0;
+    let mut has_pair = false;
+    for digit in digits(number) {
+        if prev > digit {
+            return false; // not growing
+        };
+        if prev == digit {
+            has_pair = true;
+        }
+        prev = digit;
+    }
+    has_pair
+}
+
+fn matches_part2(number: usize) -> bool {
+    let mut group = (0, 0);
+    let mut has_pair = false;
+    for digit in digits(number) {
+        group = match group {
+            (prev, count) => {
+                if prev > digit {
+                    return false; // not growing
+                };
+                if prev == digit {
+                    (prev, count + 1)
+                } else {
+                    if count == 2 {
+                        has_pair = true;
+                    }
+                    (digit, 1)
+                }
+            }
+        }
+    }
+    has_pair || (group.1 == 2)
 }
 
 fn digits(mut number: usize) -> Vec<usize> {
@@ -49,14 +77,26 @@ mod spec {
     }
 
     #[test]
-    fn examples() {
-        assert_eq!(matches_password(111111), true);
-        assert_eq!(matches_password(223450), false);
-        assert_eq!(matches_password(123789), false);
+    fn examples_part1() {
+        assert_eq!(matches_part1(111111), true);
+        assert_eq!(matches_part1(223450), false, "not growing");
+        assert_eq!(matches_part1(123789), false, "does not have pair");
+    }
+    #[test]
+    fn examples_part2() {
+        assert_eq!(matches_part2(123456), false, "does not have pair");
+        assert_eq!(matches_part2(112233), true, "growing groups");
+        assert_eq!(matches_part2(123444), false, "more than 2 in group");
+        assert_eq!(matches_part2(111122), true, "4 elements are 2 groups");
     }
 
     #[test]
     fn part1_my_input() {
         assert_eq!(part1(INPUT), 1625);
+    }
+
+    #[test]
+    fn part2_my_input() {
+        assert_eq!(part2(INPUT), 1111);
     }
 }
