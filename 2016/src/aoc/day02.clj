f2c7cae3e23bb7075bdbc470f0da127597f423e6
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-11 21:21:32 +0000

    2016 day2 part2

--- 2016/src/aoc/day02.clj
+++ 2016/src/aoc/day02.clj
@@ -1,26 +1,40 @@
 (ns aoc.day02
   (:require [clojure.string :as cs]))
 
-(def keyboard
-  [[1 2 3]
-   [4 5 6]
-   [7 8 9]])
+(def simple-keyboard
+  [[\1 \2 \3]
+   [\4 \5 \6]
+   [\7 \8 \9]])
 
-(defn digit [[x y]]
+(def complex-keyboard
+  [[nil nil \1 nil nil]
+   [nil \2  \3 \4  nil]
+   [\5  \6  \7 \8  \9]
+   [nil \A  \B \C  nil]
+   [nil nil \D nil nil]])
+
+(defn label [keyboard [x y]]
   (get (get keyboard y) x))
 
-(defn move [[x y] direction]
-  (case direction
-    \U [x (max (- y 1) 0)]
-    \D [x (min (+ y 1) 2)]
-    \L [(max (- x 1) 0) y]
-    \R [(min (+ x 1) 2) y]))
+(defn move [keyboard [x y] direction]
+  (let [next-position (case direction
+                        \U [x (- y 1)]
+                        \D [x (+ y 1)]
+                        \L [(- x 1) y]
+                        \R [(+ x 1) y])]
+    (if (label keyboard next-position) next-position [x y])))
 
-(defn moves [position directions]
-  (reduce move position directions))
+(defn moves [keyboard position directions]
+  (reduce (partial move keyboard) position directions))
+
+(defn decode [input keyboard start]
+  (let [lines  (cs/split-lines input)
+        points (reductions (partial moves keyboard) start lines)
+        labels (drop 1 (map (partial label keyboard) points) )]
+    (apply str labels)))
 
 (defn part1 [input]
-  (let [lines  (cs/split-lines input)
-        points (reductions moves [1 1] lines)
-        digits (drop 1 (map digit points) )]
-    (reduce (fn [r a] (+ (* r 10) a)) 0 digits)))
+  (decode input simple-keyboard [1 1]))
+
+(defn part2 [input]
+  (decode input complex-keyboard [0 2]))
--- 2016/test/aoc/day02_test.clj
+++ 2016/test/aoc/day02_test.clj
@@ -1,12 +1,18 @@
 (ns aoc.day02-test
   (:require [clojure.test :refer [deftest testing is]]
-            [aoc.day02 :refer [part1]]))
+            [aoc.day02 :refer [part1 part2]]))
 
   (def input (slurp "./resources/day02.txt"))
 
 (deftest day2-part1
   (testing "examples"
-    (is (= (part1 "ULL\nRRDDD\nLURDL\nUUUUD\n") 1985)))
+    (is (= (part1 "ULL\nRRDDD\nLURDL\nUUUUD\n") "1985")))
   (testing "my input"
-    (is ( = (part1 input) 92435))))
+    (is ( = (part1 input) "92435"))))
+
+(deftest day2-part2
+  (testing "examples"
+    (is (= (part2 "ULL\nRRDDD\nLURDL\nUUUUD\n") "5DB3")))
+  (testing "my input"
+    (is ( = (part2 input) "C1A88"))))
 
