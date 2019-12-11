3088803f90387232e9eb9129b880b03c89f8e213
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-11 21:24:29 +0000

    2016 day2 bench

---
+++ 2016/bench/aoc/day02_bench.clj
@@ -0,0 +1,9 @@
+(ns aoc.day02-bench
+  (:require [libra.bench :refer [defbench is]]
+            [libra.criterium :refer [quick-bench]]
+            [aoc.day02 :refer [part1 part2]]))
+
+(def input (slurp "./resources/day02.txt"))
+
+(defbench day02-part1 (is (quick-bench (part1 input))))
+(defbench day02-part2 (is (quick-bench (part2 input))))
