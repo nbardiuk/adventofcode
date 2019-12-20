118e02f91f1133040258de0405fec9c0665839e9
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-20 13:51:17 +0000

    Day19 part2

--- 2019/src/day19.rs
+++ 2019/src/day19.rs
@@ -13,6 +13,33 @@ pub fn part1(input: &str) -> i64 {
     sum
 }
 
+pub fn part2(input: &str) -> i64 {
+    let computer = Computer::parse(input);
+    let mut first_j = 0_i64;
+    for i in 20.. {
+        let mut beem = false;
+        let mut last_j = 0;
+        for j in first_j.. {
+            let v = computer.clone().call(vec![j, i]);
+            if !beem && v == 1 {
+                first_j = j;
+                beem = true;
+            }
+            if beem && v == 0 {
+                last_j = j - 1;
+                break;
+            }
+        }
+        if last_j - first_j > 100 {
+            let corner_j = last_j - 99;
+            if computer.clone().call(vec![corner_j, i + 99]) == 1 {
+                return corner_j * 10000 + i;
+            }
+        }
+    }
+    0
+}
+
 #[cfg(test)]
 mod spec {
     use super::*;
@@ -20,5 +47,10 @@ mod spec {
     #[test]
     fn part1_my_input() {
         assert_eq!(part1(INPUT), 171);
+    }
+
+    #[test]
+    fn part2_my_input() {
+        assert_eq!(part2(INPUT), 9741242);
     }
 }
