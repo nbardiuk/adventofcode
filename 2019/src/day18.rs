d44a4943a71184d2f1ae554777d7e20f2d693bc3
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-20 11:06:54 +0000

    Day18 cleanup

--- 2019/src/day18.rs
+++ 2019/src/day18.rs
@@ -6,206 +6,33 @@ use std::collections::VecDeque;
 
 pub const INPUT: &str = include_str!("../res/day18.txt");
 
-fn distance(
-    start: (i64, i64),
-    finish: (i64, i64),
-    walls: &HashSet<(i64, i64)>,
-    items: &HashMap<(i64, i64), char>,
-) -> Option<(usize, BitSet)> {
-    let mut seen = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_front((0, start, BitSet::default()));
-    while let Some((distance, (x, y), between)) = queue.pop_front() {
-        let distance = distance + 1;
-        for (i, j) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            let neighbour = (x + i, y + j);
-
-            if neighbour == finish {
-                return Some((distance, between));
-            }
-
-            if walls.contains(&neighbour) || seen.contains(&neighbour) {
-                continue;
-            }
-
-            let mut between = between;
-            if let Some(&ch) = items.get(&neighbour) {
-                if ch != '@' && !ch.is_digit(10) {
-                    between.add(ch.to_ascii_lowercase() as u8 - 'a' as u8);
-                }
-            }
-
-            seen.insert(neighbour);
-            queue.push_back((distance, neighbour, between));
-        }
-    }
-    None
-}
-
-pub fn part2(input: &str) -> usize {
-    let mut walls = HashSet::new();
-    let mut start = (0, 0);
-    let mut items = HashMap::new();
-    for (y, line) in input.lines().enumerate() {
-        for (x, v) in line.chars().enumerate() {
-            let position = (x as i64, y as i64);
-            if "#".contains(v) {
-                walls.insert(position);
-            } else if "@".contains(v) {
-                start = position;
-                items.insert(position, v);
-            } else if !".\n".contains(v) {
-                items.insert(position, v);
-            }
-        }
-    }
-
-    for (i, j) in &[(-1, 0), (1, 0), (0, 1), (0, -1), (0, 0)] {
-        let n = (start.0 + i, start.1 + j);
-        walls.insert(n);
-    }
-
-    items.remove(&start);
-    for (r, (i, j)) in [(-1, -1), (1, 1), (-1, 1), (1, -1)].iter().enumerate() {
-        let n = (start.0 + i, start.1 + j);
-        items.insert(n, ('0' as u8 + r as u8) as char);
-    }
-
-    let mut distances: HashMap<char, HashMap<char, (usize, BitSet)>> = HashMap::new();
-    for (a_pos, a) in items.iter() {
-        if !(a.is_ascii_lowercase() || a.is_digit(10)) {
-            continue;
-        }
-        for (b_pos, b) in items.iter() {
-            if a == b
-                || !(b.is_ascii_lowercase() || b.is_digit(10))
-                || distances.get(a).filter(|m| m.contains_key(b)).is_some()
-            {
-                continue;
-            }
-            if let Some((distance, between)) = distance(*a_pos, *b_pos, &walls, &items) {
-                distances
-                    .entry(*a)
-                    .or_default()
-                    .insert(*b, (distance, between));
-                distances
-                    .entry(*b)
-                    .or_default()
-                    .insert(*a, (distance, between));
-            }
-        }
-    }
-
-    let keys_count = items.values().filter(|&c| c.is_ascii_lowercase()).count();
-
-    let mut best = HashMap::new();
-    let mut queue = BinaryHeap::new();
-    queue.push((
-        Reverse(0_usize),
-        Reverse(0_usize),
-        vec!['0', '1', '2', '3'],
-        BitSet::default(),
-    ));
-    while let Some((_, Reverse(dist_u), us, keys)) = queue.pop() {
-        if keys.len() == keys_count {
-            return dist_u;
-        }
-        for i in 0..us.len() {
-            let u = us[i];
-            for (v, (len_u_v, between)) in distances[&u].iter() {
-                if v.is_digit(10) {
-                    continue;
-                }
-                if !keys.contains_all(*between) {
-                    continue;
-                }
-                if keys.contains(*v as u8 - 'a' as u8) {
-                    continue;
-                }
-                let mut keys = keys;
-                if !v.is_digit(10) {
-                    keys.add(*v as u8 - 'a' as u8);
-                }
-                let mut us = us.clone();
-                us[i] = *v;
-                let next = (us.clone(), keys);
-                let alternative = dist_u.saturating_add(*len_u_v);
-                if alternative < *best.get(&next).unwrap_or(&std::usize::MAX) {
-                    best.insert(next, alternative);
-                    queue.push((Reverse(keys.len()), Reverse(alternative), us, keys));
-                }
-            }
-        }
-    }
-    0
-}
 pub fn part1(input: &str) -> usize {
-    let mut walls = HashSet::new();
-    let mut start = (0, 0);
-    let mut items = HashMap::new();
-    for (y, line) in input.lines().enumerate() {
-        for (x, v) in line.chars().enumerate() {
-            let position = (x as i64, y as i64);
-            if "#".contains(v) {
-                walls.insert(position);
-            } else if "@".contains(v) {
-                start = position;
-                items.insert(position, v);
-            } else if !".\n".contains(v) {
-                items.insert(position, v);
-            }
-        }
-    }
+    let (walls, mut items) = parse_input(input);
+    let start = *items.iter().find(|(_, v)| v == &&'@').unwrap().0;
 
-    let mut distances: HashMap<char, HashMap<char, (usize, BitSet)>> = HashMap::new();
-    for (a_pos, a) in items.iter() {
-        if !(a.is_ascii_lowercase() || a == &'@') {
-            continue;
-        }
-        for (b_pos, b) in items.iter() {
-            if a == b
-                || !(b.is_ascii_lowercase() || b == &'@')
-                || distances.get(a).filter(|m| m.contains_key(b)).is_some()
-            {
-                continue;
-            }
-            if let Some((distance, between)) = distance(*a_pos, *b_pos, &walls, &items) {
-                distances
-                    .entry(*a)
-                    .or_default()
-                    .insert(*b, (distance, between));
-                distances
-                    .entry(*b)
-                    .or_default()
-                    .insert(*a, (distance, between));
-            }
-        }
-    }
-
-    items.remove(&start);
+    items.insert(start, '0');
+    let distances = distances_between(&items, &walls);
     let keys_count = items.values().filter(|&c| c.is_ascii_lowercase()).count();
 
     let mut best = HashMap::new();
     let mut queue = BinaryHeap::new();
-    queue.push((Reverse(0), '@', BitSet::default()));
-    while let Some((Reverse(dist_u), u, mut keys)) = queue.pop() {
-        if u != '@' {
-            keys.add(u as u8 - 'a' as u8);
-        }
+    queue.push((Reverse(0), '0', BitSet::default()));
+    while let Some((Reverse(dist_u), u, keys)) = queue.pop() {
         if keys.len() == keys_count {
             return dist_u;
         }
+        for (v, (len_u_v, required)) in distances[&u].iter() {
+            if !keys.contains_all(*required) {
+                continue;
+            }
 
-        for (v, (len_u_v, between)) in distances[&u].iter() {
-            if v == &'@' {
+            let key = key(*v);
+            if keys.contains(key) {
                 continue;
             }
-            if !keys.contains_all(*between) {
-                continue;
-            }
-            if keys.contains(*v as u8 - 'a' as u8) {
-                continue;
-            }
+            let mut keys = keys;
+            keys.add(key);
+
             let alternative = dist_u.saturating_add(*len_u_v);
             if alternative < *best.get(&(*v, keys)).unwrap_or(&std::usize::MAX) {
                 best.insert((*v, keys), alternative);
@@ -215,18 +42,129 @@ pub fn part1(input: &str) -> usize {
     }
     0
 }
+pub fn part2(input: &str) -> usize {
+    let (mut walls, mut items) = parse_input(input);
+    let start = *items.iter().find(|(_, v)| v == &&'@').unwrap().0;
+
+    for (i, j) in &[(-1, 0), (1, 0), (0, 1), (0, -1), (0, 0)] {
+        walls.insert((start.0 + i, start.1 + j));
+    }
+
+    items.remove(&start);
+    for (robot, i, j) in &[('0', -1, -1), ('1', 1, 1), ('2', -1, 1), ('3', 1, -1)] {
+        items.insert((start.0 + i, start.1 + j), *robot);
+    }
+
+    let distances = distances_between(&items, &walls);
+    let keys_count = items.values().filter(|&c| c.is_ascii_lowercase()).count();
+
+    let mut best = HashMap::new();
+    let mut queue = BinaryHeap::new();
+    queue.push((Reverse(0_usize), ['0', '1', '2', '3'], BitSet::default()));
+    while let Some((Reverse(dist_u), us, keys)) = queue.pop() {
+        if keys.len() == keys_count {
+            return dist_u;
+        }
+        for i in 0..us.len() {
+            let u = us[i];
+            for (v, (len_u_v, required)) in distances[&u].iter() {
+                if !keys.contains_all(*required) {
+                    continue;
+                }
+
+                let key = key(*v);
+                if keys.contains(key) {
+                    continue;
+                }
+                let mut keys = keys;
+                keys.add(key);
+
+                let mut us = us;
+                us[i] = *v;
+
+                let alternative = dist_u.saturating_add(*len_u_v);
+                if alternative < *best.get(&(us, keys)).unwrap_or(&std::usize::MAX) {
+                    best.insert((us, keys), alternative);
+                    queue.push((Reverse(alternative), us, keys));
+                }
+            }
+        }
+    }
+    0
+}
+
+fn parse_input(input: &str) -> (HashSet<(i64, i64)>, HashMap<(i64, i64), char>) {
+    let mut walls = HashSet::new();
+    let mut items = HashMap::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, v) in line.chars().enumerate() {
+            let position = (x as i64, y as i64);
+            if "#".contains(v) {
+                walls.insert(position);
+            } else if !".\n".contains(v) {
+                items.insert(position, v);
+            }
+        }
+    }
+    (walls, items)
+}
+
+fn distances_between(
+    items: &HashMap<(i64, i64), char>,
+    walls: &HashSet<(i64, i64)>,
+) -> HashMap<char, HashMap<char, (usize, BitSet)>> {
+    items
+        .iter()
+        .filter(|(_, ch)| ch.is_ascii_lowercase() || ch.is_digit(10))
+        .map(|(pos, ch)| (*ch, distances_from(*pos, walls, items)))
+        .collect()
+}
+
+fn distances_from(
+    start: (i64, i64),
+    walls: &HashSet<(i64, i64)>,
+    items: &HashMap<(i64, i64), char>,
+) -> HashMap<char, (usize, BitSet)> {
+    let mut seen = HashSet::new();
+    let mut distances = HashMap::new();
+    let mut queue = VecDeque::new();
+    queue.push_front((0, start, BitSet::default()));
+    while let Some((distance, (x, y), required)) = queue.pop_front() {
+        let distance = distance + 1;
+        for (i, j) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let neighbour = (x + i, y + j);
+
+            if walls.contains(&neighbour) || seen.contains(&neighbour) {
+                continue;
+            }
+
+            let mut required = required;
+            if let Some(&ch) = items.get(&neighbour) {
+                if !ch.is_digit(10) {
+                    if ch.is_ascii_lowercase() {
+                        distances.insert(ch, (distance, required));
+                    }
+                    required.add(key(ch));
+                }
+            }
+
+            seen.insert(neighbour);
+            queue.push_back((distance, neighbour, required));
+        }
+    }
+    distances
+}
 
 #[derive(Default, Debug, PartialEq, Hash, Eq, Clone, Copy, Ord, PartialOrd)]
 struct BitSet {
     bits: u32,
 }
-
 impl BitSet {
-    fn contains(&self, key: u8) -> bool {
+    fn contains(self, key: u8) -> bool {
         (self.bits >> key) & 1 == 1
     }
 
-    fn contains_all(&self, other: Self) -> bool {
+    fn contains_all(self, other: Self) -> bool {
         self.bits & other.bits == other.bits
     }
 
@@ -234,9 +172,12 @@ impl BitSet {
         self.bits |= 1_u32 << key;
     }
 
-    fn len(&self) -> usize {
+    fn len(self) -> usize {
         self.bits.count_ones() as usize
     }
+}
+fn key(ch: char) -> u8 {
+    ch.to_ascii_lowercase() as u8 - 'a' as u8
 }
 
 #[cfg(test)]
@@ -244,7 +185,7 @@ mod spec {
     use super::*;
 
     #[test]
-    fn example_8() {
+    fn part1_8() {
         assert_eq!(
             part1(
                 "#########\n\
@@ -256,7 +197,7 @@ mod spec {
     }
 
     #[test]
-    fn example_86() {
+    fn part1_86() {
         assert_eq!(
             part1(
                 "########################\n\
@@ -270,7 +211,7 @@ mod spec {
     }
 
     #[test]
-    fn example_132() {
+    fn part1_132() {
         assert_eq!(
             part1(
                 "########################\n\
@@ -284,7 +225,7 @@ mod spec {
     }
 
     #[test]
-    fn example2_8() {
+    fn part2_8() {
         assert_eq!(
             part2(
                 "#######\n\
@@ -300,7 +241,7 @@ mod spec {
     }
 
     #[test]
-    fn example2_24() {
+    fn part2_24() {
         assert_eq!(
             part2(
                 "###############\n\
@@ -315,7 +256,7 @@ mod spec {
         );
     }
     #[test]
-    fn example2_32() {
+    fn part2_32() {
         assert_eq!(
             part2(
                 "#############\n\
@@ -331,7 +272,7 @@ mod spec {
     }
 
     #[test]
-    fn example2_72() {
+    fn part2_72() {
         assert_eq!(
             part2(
                 "#############\n\
