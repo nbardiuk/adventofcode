2087072f3319fe02b5349882ede69c3f577c88b0
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-21 06:56:04 +0000

    Day21 part2

--- 2019/src/day21.rs
+++ 2019/src/day21.rs
@@ -14,11 +14,34 @@ WALK
     Computer::parse(input).call(program.chars().map(|c| (c as u8) as i64).collect())
 }
 
+pub fn part2(input: &str) -> i64 {
+    let program = "\
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+NOT H T
+AND E T
+OR H T
+AND T J
+AND D J
+RUN
+";
+    Computer::parse(input).call(program.chars().map(|c| (c as u8) as i64).collect())
+}
+
 #[cfg(test)]
 mod spec {
     use super::*;
+
     #[test]
     fn part1_my_input() {
         assert_eq!(part1(INPUT), 19358416);
+    }
+
+    #[test]
+    fn part2_my_input() {
+        assert_eq!(part2(INPUT), 1144641747);
     }
 }
