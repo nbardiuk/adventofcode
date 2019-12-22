34489b4c3d2cc12eecb16f7d42abd806451db71a
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-12-22 14:03:55 +0000

    Day22 cleanup

--- 2019/src/day22.rs
+++ 2019/src/day22.rs
@@ -1,90 +1,72 @@
-use num_bigint::*;
-
 pub const INPUT: &str = include_str!("../res/day22.txt");
 
 #[derive(Debug)]
 enum Command {
     IntoStack,
-    WithIncrement(i64),
-    Cut(i64),
+    WithIncrement(i32),
+    Cut(i32),
 }
+use Command::*;
 
-pub fn part1(input: &str) -> i64 {
-    let commands = input
-        .lines()
-        .map(|line| {
-            if line.starts_with("deal with increment") {
-                let rest = line[20..].parse().unwrap();
-                Command::WithIncrement(rest)
-            } else if line.starts_with("cut") {
-                let rest = line[4..].parse().unwrap();
-                Command::Cut(rest)
-            } else {
-                Command::IntoStack
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let mut card = 2019;
+pub fn part1(input: &str) -> i32 {
     let size = 10007;
-    for command in commands {
-        match command {
-            Command::IntoStack => card = (size - 1) - card,
-            Command::Cut(index) => card = (card + size - index) % size,
-            Command::WithIncrement(increment) => card = (card * increment) % size,
+    let mut card = 2019;
+    for command in parse_commands(input) {
+        card = match command {
+            IntoStack => (size - 1) - card,
+            Cut(index) => (card + size - index) % size,
+            WithIncrement(increment) => (card * increment) % size,
         }
     }
     card
 }
 
-pub fn part2(input: &str) -> i64 {
-    let commands = input
-        .lines()
-        .map(|line| {
-            if line.starts_with("deal with increment") {
-                let rest = line[20..].parse().unwrap();
-                Command::WithIncrement(rest)
-            } else if line.starts_with("cut") {
-                let rest = line[4..].parse().unwrap();
-                Command::Cut(rest)
-            } else {
-                Command::IntoStack
-            }
-        })
-        .collect::<Vec<_>>();
+pub fn part2(input: &str) -> i128 {
+    let size = 119_315_717_514_047_i128;
+    let add = |a, b| (a + b) % size;
+    let mul = |a, b| (a * b) % size;
+    let pow = |mut base: i128, mut exp: i128| {
+        let mut result = 1;
+        while exp > 0 {
+            if exp & 1 == 1 {
+                result = mul(result, base)
+            };
+            base = mul(base, base);
+            exp >>= 1;
+        }
+        result
+    };
 
-    let size = ToBigInt::to_bigint(&119_315_717_514_047_usize).unwrap();
-    let mut a = ToBigInt::to_bigint(&1).unwrap();
-    let mut b = ToBigInt::to_bigint(&0).unwrap();
-    for command in commands.iter().rev() {
+    let mut a = 1;
+    let mut b = 0;
+    for command in parse_commands(input).rev() {
         match command {
-            Command::IntoStack => {
-                b += 1;
-                a *= -1;
-                b *= -1;
+            IntoStack => {
+                a = mul(a, -1);
+                b = mul(b + 1, -1);
             }
-            Command::Cut(index) => b = b + index,
-            Command::WithIncrement(increment) => {
-                let p = ToBigInt::to_bigint(increment)
-                    .unwrap()
-                    .modpow(&(&size - 2), &size);
-                a *= &p;
-                b *= &p;
+            Cut(index) => b = add(b, index as i128),
+            WithIncrement(increment) => {
+                let p = pow(increment as i128, size - 2);
+                a = mul(a, p);
+                b = mul(b, p);
             }
         }
-        a %= &size;
-        b %= &size;
     }
 
-    let x = 2020;
-    let n = ToBigInt::to_bigint(&101_741_582_076_661_usize).unwrap();
+    let n = 101_741_582_076_661_i128;
+    add(
+        mul(pow(a, n), 2020),
+        mul(mul(b, add(pow(a, n), size - 1)), pow(a - 1, size - 2)),
+    )
+}
 
-    let pow = |a: &BigInt, n: &BigInt| a.modpow(n, &size);
-
-    let result: BigInt =
-        (pow(&a, &n) * x + b * (pow(&a, &n) + &size - 1) * pow(&(&a - 1), &(&size - 2))) % &size;
-
-    result.to_str_radix(10).parse().unwrap()
+fn parse_commands<'a>(input: &'a str) -> impl DoubleEndedIterator<Item = Command> + 'a {
+    input.lines().map(|line| match &line[0..6] {
+        "deal w" => WithIncrement(line[20..].parse().unwrap()),
+        "deal i" => IntoStack,
+        _ => Cut(line[4..].parse().unwrap()),
+    })
 }
 
 #[cfg(test)]
