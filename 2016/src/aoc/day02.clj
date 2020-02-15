89dc92bcea0ed70f587e07ca8c5a517630322155
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-02-15 13:06:52 +0000

    2016 day2 refactoring

--- 2016/src/aoc/day02.clj
+++ 2016/src/aoc/day02.clj
@@ -1,40 +1,53 @@
 (ns aoc.day02
-  (:require [clojure.string :as cs]))
+  (:require [clojure.string :as s]))
 
-(def simple-keyboard
+(def simple-keypad
   [[\1 \2 \3]
    [\4 \5 \6]
    [\7 \8 \9]])
 
-(def complex-keyboard
+(def complex-keypad
   [[nil nil \1 nil nil]
    [nil \2  \3 \4  nil]
    [\5  \6  \7 \8  \9]
    [nil \A  \B \C  nil]
    [nil nil \D nil nil]])
 
-(defn label [board [x y]]
-  (-> board (get y) (get x)))
+(defn- label [keypad {:keys [x y]}]
+  (-> keypad (get y) (get x)))
 
-(defn move [board [x y :as position] direction]
-  (let [candidate (case direction
-                    \U [x (- y 1)]
-                    \D [x (+ y 1)]
-                    \L [(- x 1) y]
-                    \R [(+ x 1) y])]
-    (if (label board candidate) candidate position)))
+(defn- button [keypad position]
+  (some->> position
+           (label keypad)
+           (assoc {:position position} :label)))
 
-(defn moves [board position directions]
-  (reduce (partial move board) position directions))
+(defn- neighbour [{:keys [x y]} direction]
+  (case direction
+    \U {:x x       :y (- y 1)}
+    \D {:x x       :y (+ y 1)}
+    \L {:x (- x 1) :y y}
+    \R {:x (+ x 1) :y y}))
 
-(defn decode [input board start]
-  (let [lines  (cs/split-lines input)
-        points (reductions (partial moves board) start lines)
-        labels (drop 1 (map (partial label board) points))]
-    (apply str labels)))
+(defn- move [keypad {:keys [position] :as current} direction]
+  (or (button keypad (neighbour position direction))
+      current))
+
+(defn- moves [keypad button instruction]
+  (reduce (partial move keypad) button instruction))
+
+(defn- buttons [keypad start instructions]
+  (reductions (partial moves keypad) (button keypad start) instructions))
+
+(defn- decode [input keypad start]
+  (->> input
+       s/split-lines
+       (buttons keypad start)
+       (map :label)
+       (drop 1)
+       (apply str)))
 
 (defn part1 [input]
-  (decode input simple-keyboard [1 1]))
+  (decode input simple-keypad {:x 1 :y 1}))
 
 (defn part2 [input]
-  (decode input complex-keyboard [0 2]))
+  (decode input complex-keypad {:x 0 :y 2}))
