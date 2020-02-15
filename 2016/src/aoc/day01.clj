31c686659cd8a0b99197c3be9f1a25d4a422851d
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-02-15 11:46:23 +0000

    2016 day1 refactoring

--- 2016/src/aoc/day01.clj
+++ 2016/src/aoc/day01.clj
@@ -1,50 +1,60 @@
 (ns aoc.day01
   (:require [clojure.string :as cs]))
 
-(defn words [line] (cs/split line #", "))
+(defn- words [line] (cs/split line #", "))
 
-(defn parse-command [word]
+(defn- parse-command [word]
   (let [[direction distance] (split-at 1 word)]
-    [(apply str direction) (read-string (apply str distance))]))
+    {:direction (apply str direction)
+     :distance (read-string (apply str distance))}))
 
-(defn parse-commands [line]
+(defn- parse-commands [line]
   (map parse-command (words line)))
 
-(defn navigate [[[x y] [i j]] [direction distance]]
+(defn- navigate
+  [{:keys [x y i j]}
+   {:keys [direction distance]}]
   (let [[i j] (case direction
                 "L" [(- j) i]
                 "R" [j (- i)])
         x (+ x (* distance i))
         y (+ y (* distance j))]
-    [[x y] [i j]]))
+    {:x x :y y :i i :j j}))
 
-(defn distance [[x y]]
+(defn- distance [{:keys [x y]}]
   (+ (Math/abs x) (Math/abs y)))
 
 (defn part1 [input]
-  (let [commands (parse-commands input)
-        [position] (reduce navigate [[0 0] [0 -1]] commands)]
-    (distance position)))
+  (->> input
+       parse-commands
+       (reduce navigate {:x 0 :y 0 :i 0 :j -1})
+       distance))
 
-(defn xrange [from to] (range from to (if (> from to) -1 1)))
+(defn- xrange [from to]
+  (range from to (if (> from to) -1 1)))
 
-(defn track [[result [from-x from-y]] [to-x to-y]]
-  (let [trace (if (== from-y to-y)
-                (map #(-> [% from-y]) (xrange from-x to-x))
-                (map #(-> [from-x %]) (xrange from-y to-y)))]
-    [(concat result trace) [to-x to-y]]))
+(defn- trace [{from-x :x from-y :y} {to-x :x to-y :y}]
+  (if (== from-y to-y)
+    (map #(array-map :x %1 :y %2) (xrange from-x to-x) (repeat from-y))
+    (map #(array-map :x %1 :y %2) (repeat from-x) (xrange from-y to-y))))
 
-(defn seen? [[x xs]] (contains? xs x))
+(defn- tracing [{:keys [path from]} to]
+  {:path (concat path (trace from to))
+   :from to})
 
-(defn first-duplicate [items]
-  (let [inits (reductions conj #{} items)
-        with-inits (map vector items inits)
-        [result] (first (filter seen? with-inits))]
-    result))
+(defn- contains [[x xs]] (xs x))
+
+(defn- first-duplicate [items]
+  (->> items
+       (reductions conj #{})
+       (map vector items)
+       (some contains)))
 
 (defn part2 [input]
-  (let [commands (parse-commands input)
-        navigation (reductions navigate [[0 0] [0 -1]] commands)
-        turns (map first navigation)
-        [traces] (reduce track ['() [0 0]] turns)]
-    (distance (first-duplicate traces))))
+  (->> input
+       parse-commands
+       (reductions navigate {:x 0 :y 0 :i 0 :j -1})
+       (reduce tracing {:from {:x 0 :y 0}})
+       :path
+       first-duplicate
+       distance))
