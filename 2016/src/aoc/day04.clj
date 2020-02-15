cde1b535dc5d42c540fe089140c1a49461a53e51
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-02-15 14:34:40 +0000

    2016 day4 refactoring

--- 2016/src/aoc/day04.clj
+++ 2016/src/aoc/day04.clj
@@ -1,37 +1,58 @@
 (ns aoc.day04
-  (:require [clojure.string :as cs]))
+  (:require [clojure.string :as s]))
 
-(defn read-room [line]
-  (let [[_ encoded sector checksum] (re-matches #"([a-z\-]+)-(\d+)\[(\w+)\]" line)]
-    [encoded (read-string sector) checksum]))
+(defn- read-room [line]
+  (let [[_ encrypted-name sector checksum] (re-matches #"([a-z\-]+)-(\d+)\[(\w+)\]" line)]
+    {:encrypted-name encrypted-name
+     :sector (read-string sector)
+     :checksum checksum}))
 
-(defn real? [[encoded _ checksum]]
-  (let [counts (frequencies (filter #(not= \- %) encoded))
-        sorted (sort-by (fn [[letter cnt]] [(- cnt) letter]) counts)
-        code (apply str (take 5 (map first sorted)))]
-    (= checksum code)))
+(defn- sort-by-frequency [letters]
+  (->> letters
+       frequencies
+       (sort-by (fn [[letter freq]] [(- freq) letter]))
+       (map first)))
 
-(defn decrypt [shift word]
-  (let [a (int \a)
-        letters (map #(-> % int (- a) (+ shift) (mod 26) (+ a) char) word)]
-    (apply str letters)))
+(defn- real? [{:keys [encrypted-name checksum]}]
+  (->> encrypted-name
+       (filter #(not= \- %))
+       sort-by-frequency
+       (take 5)
+       (apply str)
+       (= checksum)))
 
-(defn decrypt-message [[encoded sector _]]
-  (let [words (cs/split encoded #"-")
-        message (cs/join " " (map (partial decrypt sector) words))]
-    [message sector]))
+(defn- real-rooms [input]
+  (->> input
+       s/split-lines
+       (map read-room)
+       (filter real?)))
+
+(defn- seq-find [p xs]
+  (->> xs (filter p) first))
+
+(defn- rotate [shift letter]
+  (let [a (int \a)]
+    (-> letter int (- a) (+ shift) (mod 26) (+ a) char)))
+
+(defn- decrypt [shift word]
+  (->> word
+       (map (partial rotate shift))
+       (apply str)))
+
+(defn- room-name [{:keys [encrypted-name sector]}]
+  (s/replace encrypted-name #"[a-z]+" (partial decrypt sector)))
+
+(defn- has-north-pole [room]
+  (->> room room-name (re-find #"northpole")))
 
 (defn part1 [input]
-  (let [lines (cs/split-lines input)
-        rooms (map read-room lines)
-        real-rooms (filter real? rooms)
-        sectors (map second real-rooms)]
-    (apply + sectors)))
+  (->> input
+       real-rooms
+       (map :sector)
+       (apply +)))
 
 (defn part2 [input]
-  (let [lines (cs/split-lines input)
-        rooms (map read-room lines)
-        real-rooms (filter real? rooms)
-        decrypted (map decrypt-message real-rooms)
-        north-pole (filter #(.contains (first  %) "northpole") decrypted)]
-    (second (first north-pole))))
+  (->> input
+       real-rooms
+       (seq-find has-north-pole)
+       :sector))
