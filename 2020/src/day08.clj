7a65f3ea75e9fc33fa31dbcd4e75e2ab837090f1
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-12-08 06:59:42 +0000

    2020 day 8 more cleanup

--- 2020/src/day08.clj
+++ 2020/src/day08.clj
@@ -1,44 +1,44 @@
 (ns day08
   (:require [clojure.string :as str]))
 
-(defn read-instructions [input]
+(defn read-program [input]
   (->> (str/split-lines input)
        (map #(str/split % #" "))
-       (map #(update % 1 read-string))
-       vec))
+       (mapv (fn [[op arg]]
+               [(keyword op) (read-string arg)]))))
 
 (defn flip [[op arg :as instruction]]
   (case op
-    "acc" instruction
-    "jmp" ["nop" arg]
-    "nop" ["jmp" arg]))
+    :acc instruction
+    :jmp [:nop arg]
+    :nop [:jmp arg]))
 
-(defn flip-seq [instructions]
-  (->> (range)
-       (map #(update instructions % flip))
-       (take (count instructions))
+(defn flip-seq [program]
+  (->> (range (count program))
+       (map #(update program % flip))
        distinct))
 
-(defn execute [instructions]
-  (loop [acc 0
-         i 0
+(defn execute [program]
+  (loop [accumulator 0
+         counter 0
          seen #{}]
     (cond
-      (seen i) {:loops acc}
-      (<= (count instructions) i) {:halts acc}
-      :else (let [[op arg] (get instructions i)]
+      (seen counter) {:loops accumulator}
+      (= counter (count program)) {:halts accumulator}
+      :else (let [[op arg] (get program counter)
+                  seen (conj seen counter)]
               (case op
-                "acc" (recur (+ arg acc) (inc i) (conj seen i))
-                "jmp" (recur acc (+ arg i) (conj seen i))
-                "nop" (recur acc (inc i) (conj seen i)))))))
+                :acc (recur (+ arg accumulator) (inc counter) seen)
+                :jmp (recur accumulator (+ arg counter) seen)
+                :nop (recur accumulator (inc counter) seen))))))
 
 (defn part1 [input]
-  (->> (read-instructions input)
+  (->> (read-program input)
        execute
        :loops))
 
 (defn part2 [input]
-  (->> (read-instructions input)
+  (->> (read-program input)
        flip-seq
        (map execute)
        (some :halts)))
