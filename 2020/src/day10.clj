41ab927d45ea660f2af9a3abe067a54ee3578d8d
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-12-10 18:32:03 +0000

    2020 day 10

--- 2020/src/day10.clj
+++ 2020/src/day10.clj
@@ -6,40 +6,32 @@
        (map read-string)))
 
 (defn- with-edges [ns]
-  (into ns [0 (+ (apply max ns) 3)]))
-
-(defn connect-all [ns]
-  (->> (for [a ns
-             b ns
-             :when (<= 1 (- b a) 3)]
-         {a b})
-       (reduce (partial merge-with min) {})))
+  (conj ns 0 (+ (apply max ns) 3)))
 
 (defn count-connections [ns]
-  (let [[n & rest] (sort ns)
-        connections (filter #(<= 1 (- % n) 3) rest)
-        subs (map #(filter (partial <= %) rest) connections)]
-    (if (seq connections)
-      (apply + (map count-connections subs))
-      1)))
+  (let [n (count ns)]
+    (/ (+ (* n n) (* -3 n) 4) 2)))
 
 (defn connected-components [ns]
-  (reduce (fn [[[top-element :as top-group] & rest-groups :as groups] n]
-            (if (or (nil? top-element) (= 3 (- n top-element)))
-              (cons [n] groups)
-              (cons (cons n top-group) rest-groups)))
-          []
-          (sort ns)))
+  (let [prev (atom 0)
+        group (atom true)]
+    (->> (sort ns)
+         (partition-by
+          (fn [n]
+            (when (= 3 (- n @prev))
+              (swap! group not))
+            (reset! prev n)
+            @group)))))
 
 (defn part1 [input]
-  (letfn [(difference [[a b]] (- b a))
-          (checksum [m] (* (get m 1) (get m 3)))]
-    (->> (read-numbers input)
-         with-edges
-         connect-all
-         (map difference)
-         frequencies
-         checksum)))
+  (->> (read-numbers input)
+       with-edges
+       sort
+       (partition 2 1)
+       (map (fn [[a b]] (- b a)))
+       frequencies
+       vals
+       (apply *)))
 
 (defn part2 [input]
   (->> (read-numbers input)
