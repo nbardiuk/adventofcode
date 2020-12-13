16f83c35f14498e4709b6ba494a63c91c5b3f928
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-12-13 08:39:13 +0000

    2020 day 13 cleanup

--- 2020/src/day13.clj
+++ 2020/src/day13.clj
@@ -2,33 +2,39 @@
   (:require [clojure.string :as str]
             [clojure.math.numeric-tower :as math]))
 
-(defn part1 [input]
+(defn- read-schedule [input]
   (let [[time busses] (str/split-lines input)
         time (read-string time)
         busses (map read-string (re-seq #"\d+" busses))]
+    [time busses]))
+
+(defn- read-busses [input]
+  (let [[_ busses] (str/split-lines input)]
+    (->> (str/split busses #",")
+         (keep-indexed (fn [i bus-cycle]
+                         (when-not (= "x" bus-cycle)
+                           [(read-string bus-cycle) i]))))))
+
+(defn- wait-time [time bus-cycle]
+  (mod (- bus-cycle time) bus-cycle))
+
+(defn part1 [input]
+  (let [[time busses] (read-schedule input)]
     (->> busses
-         (map #(vector (- % (rem time %)) %))
+         (map #(vector (wait-time time %) %))
          (apply min-key first)
          (apply *))))
 
 (defn part2 [input]
-  (let [[_ busses] (str/split-lines input)]
-    (->> (str/split busses #",")
+  (->> (read-busses input)
 
-         (keep-indexed
-          (fn [i bus]
-            (when-not (= "x" bus)
-              [(read-string bus) i])))
+       (reduce
+        (fn [[step result] [bus-cycle i]]
+          (let [shift (mod i bus-cycle)
+                result (->> (iterate #(+ step %) result)
+                            (filter (fn [time] (= shift (wait-time time bus-cycle))))
+                            first)
+                step (math/lcm step bus-cycle)]
+            [step result])))
 
-         (reduce
-          (fn [[result multiple] [bus i]]
-            [(->> (iterate #(+ multiple %) result)
-                  (some
-                   (fn [time]
-                     (when (= (rem i bus)
-                              (rem (- bus (rem time bus)) bus))
-                       time))))
-             (math/lcm multiple bus)])
-          [0 1])
-
-         first)))
+       second))
