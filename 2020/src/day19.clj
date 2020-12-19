1bf13f4bb1573e54ee389b928ced29220a009631
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-12-19 17:05:46 +0000

    2020 day 19 part 1 improve perfomance
    from 2s to 100ms

--- 2020/src/day19.clj
+++ 2020/src/day19.clj
@@ -1,44 +1,61 @@
 (ns day19
   (:require [clojure.string :as str]))
 
-(defn parse-input [input]
-  (let [[rules messages] (->> (str/split input #"\R\R") (map str/split-lines))]
+(defn- parse-input [input]
+  (let [[grammar messages] (->> (str/split input #"\R\R") (map str/split-lines))]
     {:messages messages
-     :rules (->> (for [rule rules
-                       :let [[index rule] (str/split rule #": ")
-                             index (read-string index)
-                             rule (if (str/starts-with? rule "\"")
-                                    (read-string rule)
-                                    (mapv #(->> % (re-seq #"\d+") (mapv read-string))
-                                          (str/split rule #"\|")))]]
-                   [index rule])
-                 (into {}))}))
+     :grammar (->> (for [rule grammar
+                         :let [[index rule] (str/split rule #": ")
+                               index (read-string index)
+                               rule (if (str/starts-with? rule "\"")
+                                      (read-string rule)
+                                      (mapv #(->> % (re-seq #"\d+") (mapv read-string))
+                                            (str/split rule #"\|")))]]
+                     [index rule])
+                   (into {}))}))
 
-(defn examples [rules i]
-  (let [rule (get rules i)]
-    (if (string? rule)
-      [rule]
-      (mapcat #(reduce
-                (fn [result i]
-                  (mapcat (fn [e] (for [r result] (str r e)))
-                          (examples rules i)))
-                [""] %)
-              rule))))
+(defn- match [grammar i s]
+  (letfn [(one [i s]
+            (let [rule (get grammar i)]
+              (if (string? rule)
+                (if (= (subs s 0 1) rule)
+                  [true (subs s 1)]
+                  [false s])
+                (any-of rule s))))
+
+          (any-of [alternation s]
+            (or (->> alternation (map #(all-of % s)) (filter first) first)
+                [false s]))
+
+          (all-of [[i & concatenation] srest]
+            (cond
+              (nil? i) [true srest]
+              (empty? srest) [false s]
+              :else (let [[ok srest] (one i srest)]
+                      (if ok
+                        (recur concatenation srest)
+                        [false s]))))]
+    (one i s)))
+
+(defn- full-match [grammar i s]
+  (let [[ok srest]  (match grammar i s)]
+    (and ok (empty? srest))))
 
 (defn part1 [input]
-  (let [{:keys [rules messages]} (parse-input input)
-        valid? (set (examples rules 0))]
-    (count (filter valid? messages))))
+  (let [{:keys [grammar messages]} (parse-input input)]
+    (count (filter #(full-match grammar 0 %) messages))))
 
 (defn part2 [input]
-  (let [{:keys [rules messages]} (parse-input input)
-        valid42? (set (examples rules 42))
-        valid31? (set (examples rules 31))
-        len (count (first valid42?))]
-    (count
-     (for [message messages
-           :let [parts (->> message (partition len) (map str/join))
-                 [matches42 matches31] (split-with valid42? parts)]
-           :when (and (every? valid31? matches31)
-                      (< 0 (count matches31) (count matches42)))]
-       message))))
+  (let [{:keys [grammar messages]} (parse-input input)
+        matches (fn [matcher s]
+                  (loop [n 0 s s]
+                    (let [[ok srest] (matcher s)]
+                      (if ok
+                        (recur (inc n) srest)
+                        [n srest]))))]
+    (->> messages
+         (filter
+          #(let [[n42 srest] (matches (partial match grammar 42) %)
+                 [n31 srest] (matches (partial match grammar 31) srest)]
+             (and (< 0 n31 n42) (empty? srest))))
+         count)))
