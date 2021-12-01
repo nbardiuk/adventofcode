37ade6b2214a1e0b8fa010f85c03fbd485fe98cd
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-01 06:39:53 +0000

    feat: 2021 day 1

--- 2021/src/day01.clj
+++ 2021/src/day01.clj
@@ -1,4 +1,32 @@
-(ns day01)
+(ns day01
+  (:require
+   [clojure.string :as string]))
+
+(defn- parse-longs [input]
+  (->> (string/split-lines input)
+       (map parse-long)))
+
+(defn- keep-increasing [numbers]
+  (->> numbers
+       (partition 2 1)
+       (filter #(apply < %))
+       (map second)))
+
+(defn- three-measurements [numbers]
+  (->> numbers
+       (partition-all 3 1)
+       (map #(apply + %))))
 
 (defn part1 [input]
-  input)
+  (->> input
+       parse-longs
+       keep-increasing
+       count))
+
+(defn part2 [input]
+  (->> input
+       parse-longs
+       three-measurements
+       keep-increasing
+       count))
+
--- 2021/test/day01_test.clj
+++ 2021/test/day01_test.clj
@@ -1,8 +1,30 @@
 (ns day01-test
   (:require
-   [clojure.test :refer [deftest testing is]]
-   [day01 :refer [part1]]))
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is testing]]
+   [day01 :refer [part1 part2]]))
+
+(def example "199
+200
+208
+210
+200
+207
+240
+269
+260
+263")
+
+(def my-input (slurp (io/resource "input1.txt")))
 
 (deftest part1-test
   (testing "example"
-    (is (= 1 (part1 "asdf")))))
+    (is (= 7 (part1 example))))
+  (testing "my input"
+    (is (= 1184 (part1 my-input)))))
+
+(deftest part2-test
+  (testing "example"
+    (is (= 5 (part2 example))))
+  (testing "my input"
+    (is (= 1158 (part2 my-input)))))
