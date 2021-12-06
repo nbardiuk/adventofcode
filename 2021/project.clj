108356d2794b4bb93379a5dfe5ebfd0a0a3b196d
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-06 16:45:12 +0000

    feat: 2021 day 6 sketch rewrite using clojure2d API

--- 2021/project.clj
+++ 2021/project.clj
@@ -5,6 +5,7 @@
   :profiles {:dev {:dependencies [[org.clj-commons/clj-http-lite "0.4.392"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [io.github.nextjournal/clerk "0.2.214"]
-                                  [quil "3.1.0"]]
+                                  [quil "3.1.0"]
+                                  [clojure2d "1.4.3"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}})
