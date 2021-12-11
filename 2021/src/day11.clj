c232b5af43d36a5a68d426a4ab12dea14035e978
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-11 08:00:47 +0000

    refactor: 2021.11 switch from vector to map grid

--- 2021/src/day11.clj
+++ 2021/src/day11.clj
@@ -1,24 +1,29 @@
-(ns day11)
+(ns day11
+  (:require [clojure.string :as string]))
 
-(defn parse-longs [input]
-  (->> input (re-seq #"\d") (mapv parse-long)))
+(defn parse-grid [input]
+  (let [lines (string/split-lines input)]
+    (->> (for [y (range (count lines))
+               :let [line (get lines y)]
+               x (range (count line))
+               :let [level (subs line x (inc x))]]
+           {[x y] (parse-long level)})
+         (apply merge))))
 
-(defn- fix-point [f x]
+(defn fix-point [f x]
   (->> (iterate f x)
        (partition 2 1)
        (drop-while #(apply not= %))
        ffirst))
 
-(defn neighbours [i]
-  (let [x (mod i 10)
-        y (quot i 10)]
-    (for [dx [-1 0 1]
-          dy [-1 0 1]
-          :let [x' (+ x dx)
-                y' (+ y dy)]
-          :when (and (<= 0 x' 9) (<= 0 y' 9)
-                     (not= [x y] [x' y']))]
-      (+ (* y' 10) x'))))
+(defn neighbours [[x y]]
+  (for [dx [-1 0 1]
+        dy [-1 0 1]
+        :let [x' (+ x dx)
+              y' (+ y dy)]
+        :when (and (<= 0 x' 9) (<= 0 y' 9)
+                   (not= [x y] [x' y']))]
+    [x' y']))
 
 (defn flash [levels]
   (reduce
@@ -28,22 +33,25 @@
             (remove #(zero? (get levels %)))
             (reduce #(update %1 %2 inc) (assoc levels pos 0)))
        levels))
-   levels (range (count levels))))
+   levels (keys levels)))
 
 (defn step [levels]
-  (->> levels (mapv inc) (fix-point flash)))
+  (->> (update-vals levels inc)
+       (fix-point flash)))
+
+(defn index-of [pred coll]
+  (->> coll
+       (keep-indexed #(when (pred %2) %1))
+       first))
 
 (defn part1 [input]
-  (->> input
-       parse-longs
+  (->> (parse-grid input)
        (iterate step)
        (take 101)
-       (mapcat #(filter zero? %))
+       (mapcat #(filter zero? (vals %)))
        count))
 
 (defn part2 [input]
-  (->> input
-       parse-longs
+  (->> (parse-grid input)
        (iterate step)
-       (keep-indexed #(when (every? zero? %2) %1))
-       first))
+       (index-of #(every? zero? (vals %)))))
