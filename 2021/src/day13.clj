5d2d8ddb3f47a64f4961459fbf71dbf11f57bbeb
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-13 09:38:30 +0000

    refactor: 2021.13 more cleanup

--- 2021/src/day13.clj
+++ 2021/src/day13.clj
@@ -1,5 +1,6 @@
 (ns day13
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.java.math :as math]))
 
 (defn parse-input [input]
   {:dots
@@ -10,28 +11,21 @@
    (for [[_ axis line] (re-seq #"(x|y)=(\d+)" input)]
      [({"x" 0 "y" 1} axis) (parse-long line)])})
 
-(defn fold-one [v line]
-  (if (<= v line)
-    v
-    (- line (- v line))))
+(defn fold-one [value line]
+  (- line (math/abs (- line value))))
 
 (defn fold [dots [axis line]]
-  (->> dots
-       (mapv #(update % axis fold-one line))
-       set))
+  (set (map #(update % axis fold-one line) dots)))
 
 (defn part1 [input]
   (let [{:keys [dots instructions]} (parse-input input)]
-    (->> (fold dots (first instructions))
-         count)))
+    (count (fold dots (first instructions)))))
 
 (defn part2 [input]
   (let [{:keys [dots instructions]} (parse-input input)
-        dots (reduce fold dots instructions)
-        max-y (->> dots (map second) (reduce max))
-        max-x (->> dots (map first) (reduce max))]
-    (->> (for [y (range (inc max-y))]
-           (for [x (range (inc max-x))]
+        dots (reduce fold dots instructions)]
+    (->> (for [y (range (inc (apply max (map second dots))))]
+           (for [x (range (inc (apply max (map first dots))))]
              (if (dots [x y]) "#" " ")))
          (map string/join)
          (string/join "\n"))))
