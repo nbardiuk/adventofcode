b61eba4d70e87915e33a5c9b69b4809ed5bba940
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-14 10:31:16 +0000

    refactor: 2021 14 more cleanups

--- 2021/src/day14.clj
+++ 2021/src/day14.clj
@@ -2,33 +2,30 @@
 
 (defn parse-input [input]
   {:polymer (re-find #"^\w+" input)
-   :rules (into {} (for [[_ pair ins] (re-seq #"(\w+) -> (\w)" input)]
-                     [(vec pair) (vec ins)]))})
+   :insertion (->> (for [[_ [a b] [c]] (re-seq #"(\w\w) -> (\w)" input)]
+                     [[a b] [a c b]])
+                   (into {}))})
 
 (def counts
   (memoize
-   (fn [rules polymer steps]
-     (letfn [(expand [[a b :as pair]]
-               (let [i (rules pair)]
-                 [(into [a] i) (into i [b])]))
-             (into-counts [result polymer]
-               (merge-with
-                +
-                (update result (first polymer) (fnil dec 1))
-                (counts rules polymer (dec steps))))]
+   (fn [insertion polymer steps]
+     (letfn [(into-counts [result polymer]
+               (merge-with +
+                           (update result (first polymer) (fnil dec 1))
+                           (counts insertion polymer (dec steps))))]
 
        (if (zero? steps)
          (frequencies polymer)
          (->> (partition 2 1 polymer)
-              (mapcat expand)
+              (map insertion)
               (reduce into-counts {})))))))
 
 (defn checksum [counts]
   (apply - (apply (juxt max min) (vals counts))))
 
 (defn solution [steps input]
-  (let [{:keys [rules polymer]} (parse-input input)]
-    (checksum (counts rules polymer steps))))
+  (let [{:keys [insertion polymer]} (parse-input input)]
+    (checksum (counts insertion polymer steps))))
 
 (def part1 (partial solution 10))
 (def part2 (partial solution 40))
